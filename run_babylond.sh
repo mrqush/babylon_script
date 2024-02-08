57c983483a4305b4c196007f0d710c83ee2301e9
Marko Vasnov <marko_vasnov@epam.com>
2024-02-08 15:19:42 +0200

    a

--- run_babylond.sh
+++ run_babylond.sh
@@ -13,6 +13,13 @@ date >> "$LOG_FILE"
 # Run the babylond command and capture both stdout and stderr, and append to the log file
 $BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
 $BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet2 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet3 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet4 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet5 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet6 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet7 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet8 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
+$BABYLOND tx epoching delegate bbnvaloper1vz7kl30jtsytwr0t365qtwvlf05hgdy39t55c7 1000000ubbn --from wallet9 --chain-id bbn-test-2 --gas-adjustment 1.4 --gas auto --fees 10ubbn -y >> "$LOG_FILE" 2>&1
 
 
 # Display a message in the log file indicating the end of the script
